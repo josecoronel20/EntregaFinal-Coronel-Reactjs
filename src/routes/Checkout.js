ea3f48eb23d3590afdec93d462c5f8f4d6046979
josecoronel20 <josecoronel20@outlook.com>
2023-09-17 21:53:45 -0300

    cambios a delete y a priceTotal

--- src/routes/Checkout.js
+++ src/routes/Checkout.js
@@ -8,17 +8,26 @@ const Checkout = () => {
   const [cart, setCart] = useContext(CartWidgetContext);
 
   const handleDelete = (e) => {
-    const deleteProductId = e.target.id;
+    const idX = e.target.id;
 
-    const updateCart = cart.filter((product) => product.id !== deleteProductId);
+    const productDelete = cart.find((product) => product.id == idX);
 
-    setCart(updateCart);
+    if (productDelete.quantity > 1) {
+      const updateCart = [...cart];
+      const productIndex = updateCart.findIndex((product) => product.id == idX);
+      productDelete.quantity = productDelete.quantity - 1;
+      updateCart[productIndex] = productDelete;
+      setCart(updateCart);
+    } else {
+      const updateCart = cart.filter((product) => product.id !== idX);
+      setCart(updateCart);
+    }
   };
 
-  const total = Math.floor(cart.reduce(
-    (acc, product) => acc + product.price * product.quantity,
-    0
-  ));
+
+  const total = Math.floor(
+    cart.reduce((acc, product) => acc + product.price * product.quantity, 0)
+  );
   return (
     <div className="checkoutContainer">
       <div className="checkout">
@@ -28,30 +37,17 @@ const Checkout = () => {
               <img src={product.src} alt="imagen de producto" />
               <h3>{product.name}</h3>
               <p className="quantity">cantidad: {product.quantity}</p>
-              <p className="price">${product.price}</p>
-              <div>
-                <svg
-                  className="x"
-                  onClick={handleDelete}
-                  id={product.id}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="35"
-                  height="35"
-                  viewBox="0 0 256 256"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M208.49 191.51a12 12 0 0 1-17 17L128 145l-63.51 63.49a12 12 0 0 1-17-17L111 128L47.51 64.49a12 12 0 0 1 17-17L128 111l63.51-63.52a12 12 0 0 1 17 17L145 128Z"
-                  />
-                </svg>
-              </div>
+              <p className="price">${product.price * product.quantity}</p>
+              <p className="x" onClick={handleDelete} id={product.id}>
+                x
+              </p>
             </div>
           ))
         ) : (
           <p>el carrito está vacío</p>
         )}
         <p>TOTAL: ${total}</p>
-        <BuyBtn/>
+        <BuyBtn />
       </div>
     </div>
   );
