3c727c8c46e1ce1a8f306e7e532756123d25c43a
josecoronel20 <josecoronel20@outlook.com>
2023-09-18 19:15:20 -0300

    corrigiendo algunos errores

--- src/components/itemDetailComponents/ItemQuantitySelector.js
+++ src/components/itemDetailComponents/ItemQuantitySelector.js
@@ -1,50 +1,45 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { CartWidgetContext } from "../../context/CartWidgetContext";
 import "../../styles/ItemDetailStyles/quantitySelector.css";
 
 const ItemQuantitySelector = ({ productId }) => {
   const [cart, setCart] = useContext(CartWidgetContext);
 
-  const [quantity, setQuantity] = useState(0);
-
-  const handlerSum = () => {
-    setQuantity(quantity + 1);
+  const rest = () => {
+    if (cart.some((item) => item.id === productId && item.quantity > 0)) {
+      setCart((currentItems) =>
+        currentItems.map((item) =>
+          item.id === productId
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+      );
+    }
   };
 
-  const handlerRest = () => {
-    if (quantity > 0) setQuantity(quantity - 1);
+  const sum = () => {
+    setCart((currentItems) =>
+      currentItems.map((item) =>
+        item.id === productId
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    );
   };
 
-  const handlerAddToCart = () => {
-
-    const updateCart = [...cart];
-    
-
-    // if (quantity > 0) {
-    //   const updateCart = [...cart];
-    //   const product = cart.find((product) => product.id === productId);
-    //   product.quantity = quantity;
-    //   updateCart.push(product);
-    //   setCart(updateCart);
-    // }
-  };
+  const quantity = cart.find((item) => item.id === productId)?.quantity || 0;
 
   return (
-    <div>
-      <div className="quantitySelector">
-        <button className="buttonsQuantity" onClick={handlerRest}>
-          -
-        </button>
-        <div className="text">
-          <p>cantidad</p>
-          <p>{quantity}</p>
-        </div>
-        <button className="buttonsQuantity" onClick={handlerSum}>
-          +
-        </button>
+    <div className="quantitySelector">
+      <button className="buttonsQuantity" onClick={rest}>
+        -
+      </button>
+      <div className="text">
+        <p>cantidad</p>
+        <p>{quantity}</p>
       </div>
-      <button className="addToCartBtn" onClick={handlerAddToCart}>
-        + Add to cart
+      <button className="buttonsQuantity" onClick={sum}>
+        +
       </button>
     </div>
   );
