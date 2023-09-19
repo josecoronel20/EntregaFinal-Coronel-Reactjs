1c64ca6f7bfafee74cf368011da0037703775f72
josecoronel20 <josecoronel20@outlook.com>
2023-09-19 16:29:52 -0300

    exito al crear el localStorage

--- src/context/CartWidgetContext.js
+++ src/context/CartWidgetContext.js
@@ -1,9 +1,20 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const CartWidgetContext = createContext(null);
 
 export const CartWidgetProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const storedCart = JSON.parse(localStorage.getItem("cart")) || [];
+    if (storedCart.length > 0) {
+      setCart(storedCart);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   return (
     <CartWidgetContext.Provider value={[cart, setCart]}>
